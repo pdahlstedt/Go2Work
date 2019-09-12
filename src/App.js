bcb8486b6e54417ce1b54d70676bb4e6c366631d
petter.d <petter.dahlstedt@outlook.com>
2019-09-12 23:49:03 +0200

    react menu

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Menu/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello again</h1>
@@ -24,4 +25,30 @@ function App() {
   );
 }
 
+function Menu() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a classname="navbar-brand" href={'http://www.google.se'}>Go2Work</a>
+      
+      {/* Collapse button */}
+      <button className="navbar-toggler navbar-light bg-primary" type="button" data-toggle="collapse" 
+              data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+              aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <button type="button" className="btn btn-outline-primary bg-light" >
+          LOGGA IN    
+        </button>    
+      
+        <button type="button" className="btn btn-outline-primary bg-light" >
+          SKAPA KONTO    
+        </button> 
+      </div>
+    </nav>
+  );
+}
+
 export default App;
+
