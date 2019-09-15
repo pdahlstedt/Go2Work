0434eebf3e7c23ea05e12482c36e2c389ff9b5ff
petter.d <petter.dahlstedt@outlook.com>
2019-09-15 15:52:57 +0200

    Added onClick + infrastructure

--- src/App.js
+++ src/App.js
@@ -2,53 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Menu/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello again</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Menu/>
+        <Page/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
 }
 
-function Menu() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a classname="navbar-brand" href={'http://www.google.se'}>Go2Work</a>
-      
-      {/* Collapse button */}
-      <button className="navbar-toggler navbar-light bg-primary" type="button" data-toggle="collapse" 
-              data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-              aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <button type="button" className="btn btn-outline-primary bg-light" >
-          LOGGA IN    
-        </button>    
-      
-        <button type="button" className="btn btn-outline-primary bg-light" >
-          SKAPA KONTO    
-        </button> 
-      </div>
-    </nav>
+    // This binding is necessary to make `this` work in the callback
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() {
+    console.log("Click");
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href={'http://www.google.se'}>Go2Work</a>
+        
+        {/* Collapse button */}
+        <button className="navbar-toggler navbar-light bg-primary" type="button" data-toggle="collapse" 
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <button type="button" className="btn btn-outline-primary bg-light" onClick={this.handleLogin} >
+            LOGGA IN    
+          </button>    
+        
+          <button type="button" className="btn btn-outline-primary bg-light" >
+            SKAPA KONTO    
+          </button> 
+        </div>
+      </nav>
+    );
+  }
+}
+
+function Page() {
+  return (
+  <div id="pageForm">
+    <p>Hello, this is a p</p>
+  </div>
   );
 }
 
 export default App;
-
